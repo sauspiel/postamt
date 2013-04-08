4a1841c6a9f9c11865af671ae94c1035dab23b53
Martin Schürrer <martin@schuerrer.org>
2013-04-08 13:35:52 +0200

    Add associations to Bar/Foo

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -1,3 +1,5 @@
 class Bar < ActiveRecord::Base
   self.table_name = 'test_bars'
+
+  has_many :foos
 end
--- app/models/foo.rb
+++ app/models/foo.rb
@@ -1,3 +1,5 @@
 class Foo < ActiveRecord::Base
   self.table_name = 'test_foos'
+
+  belongs_to :bar
 end
