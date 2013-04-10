57402dbaf6cc111e32289f8daa741dfafc988cbd
Martin Schürrer <martin@schuerrer.org>
2013-04-10 16:58:45 +0200

    Clean up

--- testapp/app/models/foo.rb
+++ testapp/app/models/foo.rb
@@ -4,10 +4,4 @@ class Foo < ActiveRecord::Base
   belongs_to :bar
 
   establish_connection "#{Rails.env}_slave1"
-
-  def self.connection
-    puts 'CCCCCCCCCC'
-    #require 'pry' ; binding.pry
-    super
-  end
 end
